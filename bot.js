ac606939fa92522ed01e786684582a2de8038ce5
zipstutogaming <33815781+zipstutogaming@users.noreply.github.com>
2018-03-18 15:52:36 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,46 +10,16 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+  if (msg.content === '?info') {
+    msg.reply('Informations:```Nom du bot: TeraBot\nCréateur: ZipsTutoGaming#8306\nVersion du bot: 1.0.1\nDate de mise à jour: 18/03/2018```');
+  }
   if (msg.content === '?ip') {
     msg.reply(' ```IP: teracube.mcpe.eu\nPort: 19144```');
   }
   if (msg.content === '?help') {
     msg.reply('Liste des commandes:```\n?info - Affiche les informations du bot\n?ip - Affiche ip et le port du serveur \n?ping - Ne sert à rien x)```');
   }
-  if (message.content.StartWish('?info')) {
-  	message.delete;
-  	message.channel.send("", {
-  		embed: {
-  			color : 0xE15306,
-  			author: {name: message.author.name},
-  			description : "",
-  			title : "Informations Bot",
-  			fields : [
-  			{
-  				name : 'Nom du bot',
-  				value : 'TeraBot',
-  				inline : true
-  			},{
-  			    name : 'Créateur',
-  				value : 'Zips  Tuto/Gaming#8306',
-  				inline : true
-  			},{
-  				name : 'Version',
-  				value : '1.0.1',
-  				inline : true
-  			},{
-  				name : 'Date de mise à jour',
-  				value : '18/03/2018',
-  				inline : true
-  		}],
-  		},
-  		  footer: {
-  		  	text : 'Message envoyé par TeraBot'
-  		  }
-  	});
-  }
-
-
+  
 });
 
 client.login('NDI0ODgzNjQyNzEyNTg4Mjg5.DY_XVQ.rboOpJydYFy7FZNRsA_ZrUWzIFg');
