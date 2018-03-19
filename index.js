a1298d5f45d3d50f5afd0512469a81a17c1ed5e1
zipstutogaming <33815781+zipstutogaming@users.noreply.github.com>
2018-03-20 00:22:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+var prefix = ("?")
+
+bot.on('ready', function(){
+	bot.user.setGame("TeraCube | ?help");
+	console.log("Connexion réussi à ${client.user.tag}!");
+});
+
+bot.login('NDI0ODgzNjQyNzEyNTg4Mjg5.DY_XVQ.rboOpJydYFy7FZNRsA_ZrUWzIFg');
+
+bot.on('message', message => {
+	if (message.content === prefix + "help"){
+		message.channel.sendMessage(" Liste des commandes: \n?help");
+	}
+	
+	if (message.content === "Salut"){
+		message.reply(" Hey :D");
+		console.log("Commande -Salut effectué !");
+	}
+});
