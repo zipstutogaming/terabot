cacace751df2f1cc9aef616a7cfc9771ad4b6ac0
zipstutogaming <33815781+zipstutogaming@users.noreply.github.com>
2018-03-20 07:07:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const bot = new Discord.Client();
 var prefix = ("?")
 
 bot.on('ready', function() {
-	bot.user.setGame('TeraCube | ?help');
 	console.log("Connexion réussi à ${client.user.tag}!");
 });
 
 bot.login('NDI0ODgzNjQyNzEyNTg4Mjg5.DY_XVQ.rboOpJydYFy7FZNRsA_ZrUWzIFg');
+bot.user.setGame('TeraCube | ?help');
 
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
 		message.channel.sendMessage(" Liste des commandes: \n?help");
 	}
 	
-	if (message.content === "Salut" or " salut" or "bonjour" or "Bonjour"){
+	if (message.content === "Salut"){
 		message.reply("Hey :D");
 		console.log("Commande -Salut effectué !");
 	}
