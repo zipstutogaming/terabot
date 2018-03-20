b8906d16e5819804fb229a82e13ff9dc203386b5
zipstutogaming <33815781+zipstutogaming@users.noreply.github.com>
2018-03-20 12:02:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var prefix = ("?")
 
 bot.on('ready', function() {
 	console.log("Connexion réussi à ${client.user.tag}!");
+	bot.user.setGame("TeraCube | ?help");
 });
 
 bot.login('NDI0ODgzNjQyNzEyNTg4Mjg5.DY_XVQ.rboOpJydYFy7FZNRsA_ZrUWzIFg');
