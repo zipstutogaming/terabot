831e34d8db83aea709ba44e90ec7d4043c5cafe0
zipstutogaming <33815781+zipstutogaming@users.noreply.github.com>
2018-03-20 16:16:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
 	bot.user.setPresence({ status: 'online', game: { name: 'TeraCube | ?help' } 
 });
 
-bot.login('process.env.TOKEN');
+bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
